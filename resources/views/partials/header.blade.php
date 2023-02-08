e663c34d2cef08763b5494de068a7fe5f93d78f1
Vincenzo Ruggiero <vincenzoru@live.it>
2023-02-08 21:59:18 +0100

    Create navigation menu with loops

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,18 @@
 <header>
-   
+    <div class="container">
+        <div class="nav-wrapper">
+          <img src="{{ Vite::asset('resources/assets/images/dc-logo.png')}}" alt="DC Logo" />
+          <ul>
+            @foreach ($headerMenu as $item)
+            <li>
+                <a href="{{ route('comic') }}">
+                  {{ $item }}
+                </a>
+            </li>
+            @endforeach
+            
+          </ul>
+        </div>
+      </div>
+    
 </header>
