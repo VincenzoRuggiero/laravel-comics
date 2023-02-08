beaca01c915e199747df77c50add9fd72b81d244
Vincenzo Ruggiero <vincenzoru@live.it>
2023-02-08 16:13:52 +0100

    Add pages routing code

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/comic', function () {
+    return view('comic');
+})->name('comic');
+
